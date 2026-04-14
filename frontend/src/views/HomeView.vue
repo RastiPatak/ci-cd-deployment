2cd56aa54ffc8a6fdaf5b3395a145a05a8cf59fc
RastiPatak <pat210600@spengergasse.at>
2026-04-14 12:11:22 +0200

    init

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+import { ref, onMounted } from "vue";
+
+const message = ref("");
+
+onMounted(async () => {
+  const res = await fetch("http://localhost:5000/api");
+  const data = await res.json();
+  message.value = data.message;
+});
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <TheWelcome />
+  </main>
+</template>
