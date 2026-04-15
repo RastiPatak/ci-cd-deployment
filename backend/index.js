fbe970cd6df4c3357b4750ce176203f782afd4ee
Semmelon <138894594+Semmelon@users.noreply.github.com>
2026-04-15 13:36:38 +0200

    jdkiujzhtg

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from backend" });
+  res.json({ message: "Hello from class" });
 });
 
 app.use(express.static("public"));
